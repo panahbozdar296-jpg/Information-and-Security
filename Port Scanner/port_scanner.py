7e0eab24882daf9e034e56d7df39206e845abe5c
panahbozdar296-jpg <panahbozdar296@gmail.com>
2026-09-22 18:45:27 +0100

    Complete port scanner project

--- Port Scanner/port_scanner.py
+++ Port Scanner/port_scanner.py
@@ -3,59 +3,86 @@ import common_ports
 
 
 def validateIP(target):
-  try:
-    socket.inet_aton(target)
-    return True
-  except socket.error:
-    return False
+    try:
+        socket.inet_aton(target)
+        return True
+    except socket.error:
+        return False
+
 
 def validateURL(target):
-  try:
-    socket.gethostbyname(target)
-    return True 
-  except socket.error:
-    return False 
+    try:
+        socket.gethostbyname(target)
+        return True
+    except socket.error:
+        return False
+
 
 def checkTarget(target):
-  if not validateURL(target):
-    if target[0].isdigit():
-      return "Error: Invalid IP address"
-    return "Error: Invalid hostname"
-  return False
+    if not validateURL(target):
+        if target and target[0].isdigit():
+            return "Error: Invalid IP address"
+        return "Error: Invalid hostname"
+    return False
+
 
 def portscan(target, port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        return True
-    except:
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+        s.close()
+        return result == 0
+    except socket.error:
         return False
-        
+
+
 def getService(ports):
-  services= dict()
-  for port in ports:
-    if port in common_ports.ports_and_services:
-      services[port] = common_ports.ports_and_services[port]
-  string = ""
-  for k, v in services.items():
-    string += str(k) + " "*5 + v + "\n"
-  return string
-  
+    services = {}
+
+    for port in ports:
+        if port in common_ports.ports_and_services:
+            services[port] = common_ports.ports_and_services[port]
+
+    string = ""
+
+    for k, v in services.items():
+        string += str(k) + " " * 5 + v + "\n"
+
+    return string
+
+
 def get_open_ports(target, port_range, verbose=False):
+    error = checkTarget(target)
+
+    if error:
+        return error
+
     open_ports = []
-    if checkTarget(target):
-      print(checkTarget(target))
-    for port in range(port_range[0], port_range[-1]):
-      if portscan(target, port):
-        open_ports.append(port)
+
+    for port in range(port_range[0], port_range[1] + 1):
+        if portscan(target, port):
+            open_ports.append(port)
 
     if verbose:
-      if validateIP(target):
-        ip = target
-        host = socket.gethostbyaddr(target)
-      else :
-        host = target
-        ip = socket.gethostbyname(target)
-      print("Open ports for " + host + " (" + ip + ")\nPORT     SERVICE\n" + getService(open_ports))
-    else:
-      return(open_ports)
+        if validateIP(target):
+            ip = target
+            try:
+                host = socket.gethostbyaddr(target)[0]
+            except socket.herror:
+                host = target
+        else:
+            host = target
+            ip = socket.gethostbyname(target)
+
+        return (
+            "Open ports for "
+            + host
+            + " ("
+            + ip
+            + ")\n"
+            + "PORT     SERVICE\n"
+            + getService(open_ports)
+        )
+
+    return open_ports
